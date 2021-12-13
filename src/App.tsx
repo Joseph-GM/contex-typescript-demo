13cbbab795f87d8ee4bf0a682e092f5942b99c6d
Joseph-GM <jkim.english75@gmail.com>
2021-12-13 11:01:53 +0900

    final

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import {ThemeContextProvider} from './context/ThemeContext'
 import {Box} from './context/Box';
 import {UserContextProvider} from './context/UserContext';
 import {User} from './context/User';
+import {Profile} from './components/auth/Profile'
+import {Private} from './components/auth/Private'
+import {List} from './components/generics/List'
+import { RandomNumber } from './ref/restriction/RandomNumber';
+import {Toast} from './ref/templateliterals/Toast'
+import { CustomButton } from './components/html/Button';
 
 function App() {
   return (
@@ -13,6 +19,38 @@ function App() {
       <UserContextProvider>
         <User />
       </UserContextProvider>
+      <Private isLoggedIn={true} component={Profile} />
+      {/* <List
+          items={['Batman', 'Superman', 'WonderWoman']}
+          onClick={(item) => console.log(item)} />
+      <List items={[1,2,3]} onClick={(item) => console.log(item)} /> */}
+      <List
+        items={[
+          {
+            id: 1,
+            first: 'Bruce',
+            last: 'Wayne',
+          },
+          {
+            id: 2,
+            first: 'Clark',
+            last: 'Kent',
+          },
+          {
+            id: 3,
+            first: 'Princess',
+            last: 'Diana',
+          },
+        ]}
+        onClick={(item) => console.log(item)} />
+        <RandomNumber value={10} isPositive />
+        <Toast position='center'/>
+        <CustomButton 
+          variant='primary'
+          onClick={()=>{console.log("clicked")}}
+        >
+          Primary Button
+        </CustomButton>
     </div>
   );
 }
---
+++ src/components/generics/List.tsx
@@ -0,0 +1,18 @@
+type ListProps<T> = {
+    items: T[]
+    onClick: (value: T) => void
+}
+
+export const List = <T extends {id: number, first: string, last: string}>({ items, onClick}: ListProps<T>) => {
+    return (
+        <div>
+            <h2>List of Items</h2>
+            {items.map((item, index) => {
+                console.log(index, item)
+                return(
+                    <div key={item.id} onClick={() => onClick(item)}>{item.first} {item.last}</div>
+                )
+            })}
+        </div>
+    )
+}
